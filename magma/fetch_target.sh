acb0b0cd42a1be83d9d1a45ab2e8dcf9ac0e6e1d
Srividya Subramanian <ssubramanian@ethz.ch>
2025-02-25 16:01:18 +0100

    Add a generic script to fetch target by version

---
+++ magma/fetch_target.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+##
+# Pre-requirements:
+# - env TARGET: path to target work dir
+# - env TARGET_NAME: name of the target
+# - env TARGET_VERSION: version of target from releases (PIONEER = latest)
+##
+
+source "$TARGET"/releases
+
+to_fetch_var="$TARGET_NAME"_"$TARGET_VERSION"
+to_fetch=${!to_fetch_var}
+
+if [[ "$to_fetch" =~ ^https://github\.com/.+/.+ ]]; then
+    git clone "$to_fetch" "$TARGET/repo"
+elif [[ "$to_fetch" =~ \.tar\.gz$ ]]; then
+    wget -O "$TARGET"/repo.tar.gz "$to_fetch"
+    mkdir "$TARGET"/repo
+    tar -xf "$TARGET"/repo.tar.gz --strip-components=1 -C "$TARGET"/repo
+else
+    echo "Unsupported link: $to_fetch"
+    exit
+fi
