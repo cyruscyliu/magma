2638db691f05709334f22a99c61f274d0ef3f580
Srividya Subramanian <ssubramanian@ethz.ch>
2025-02-26 10:58:50 +0100

    Tag automake version libxml

--- targets/libxml2/preinstall.sh
+++ targets/libxml2/preinstall.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 apt-get update && \
-    apt-get install -y git make autoconf automake libtool pkg-config zlib1g-dev \
+    apt-get install -y git make autoconf libtool pkg-config zlib1g-dev \
     	liblzma-dev
+
+# automake version tagged 10 1.16.3 for latest version of libxml
+wget http://ftp.de.debian.org/debian/pool/main/a/automake-1.16/automake_1.16.3-2_all.deb
+sudo apt install ./automake_1.16.3-2_all.deb
