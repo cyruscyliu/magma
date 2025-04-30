2fc49c30c296a05b18ae9fe8f37f947b04d2ba1b
Srividya Subramanian <ssubramanian@ethz.ch>
2025-04-30 21:19:11 +0200

    Clean up tars in PHP

--- targets/php/preinstall.sh
+++ targets/php/preinstall.sh
@@ -6,5 +6,6 @@ apt-get update && \
 
 # Install newer version of re2c needed for build
 wget https://github.com/skvadrik/re2c/releases/download/1.0.3/re2c-1.0.3.tar.gz
-tar -xzf re2c-1.0.3.tar.gz
+tar -xzf re2c-1.0.3.tar.gz && rm re2c-1.0.3.tar.gz
 cd re2c-1.0.3 && ./configure && make && make install
+cd .. && rm -r re2c-1.0.3
