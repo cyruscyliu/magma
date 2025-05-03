afb4330fd167bb74c691db2055b85b70776c7ac0
Srividya Subramanian <ssubramanian@ethz.ch>
2025-05-03 13:28:55 +0200

    Use libc++ over libstdc++ to build libfuzzer

--- fuzzers/libfuzzer/build.sh
+++ fuzzers/libfuzzer/build.sh
@@ -9,8 +9,8 @@ set -e
 # We need the version of LLVM which has the LLVMFuzzerRunDriver exposed
 cd "$FUZZER/repo/compiler-rt/lib/fuzzer"
 for f in *.cpp; do
-	clang++ -stdlib=libstdc++ -fPIC -O2 -std=c++11 $f -c &
+	clang++ -stdlib=libc++ -fPIC -O2 -std=c++20 $f -c &
 done && wait
 ar r "$OUT/libFuzzer.a" *.o
 
-clang++ $CXXFLAGS -std=c++11 -c "$FUZZER/src/driver.cpp" -fPIC -o "$OUT/driver.o"
+clang++ $CXXFLAGS -std=c++20 -c "$FUZZER/src/driver.cpp" -fPIC -o "$OUT/driver.o"
